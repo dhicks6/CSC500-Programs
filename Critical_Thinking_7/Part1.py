dba0f03ba6b188b8cae366002bcec47e77236547
dhicks6 <dhicks23@msudenver.edu>
2024-05-30 10:47:03 -0600

    Corrected the .txt file and started on the assignment

---
+++ Critical_Thinking_7/Part1.py
@@ -0,0 +1,3 @@
+Room_Num = {'CSC101': 3004, 'CSC102': 4501, 'CSC103': 6755, 'NET110': 1244, 'COM241': 1411}
+Instructors = {'CSC101': 'Haynes', 'CSC102': 'Alvarado', 'CSC103': 'Rich', 'NET110': 'Burke', 'COM241': 'Lee'}
+Meeting_Time = {'CSC101': '8:00 a.m.', 'CSC102': '9:00 a.m.', 'CSC103': '10:00 a.m.', 'NET110': '11:00 a.m.', 'COM241': '1:00 p,m,'}
